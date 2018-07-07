58ad6c9ceb9a615a59063dd3ba6df8056292974e
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-07-07 22:24:01 +1000

    sketching index

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * Amatino JS
+ * index.js
+ * 
+ * author: hugh@blinkybeach.com
+ */
+ 
+module.exports = {
+  AmatinoAlpha: require('source/amatino_alpha.js'),
+  Session: require('source/session.js')
+}
