f8b81ba094e0c346e5a5debba03c9b037d260593
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-08-10 19:31:32 +1000

    add Entry / Side

---
+++ source/entry.js
@@ -0,0 +1,52 @@
+/*
+ * Amatino JS
+ * entry.js
+ * 
+ * author: hugh@blinkybeach.com
+ */
+
+const Side = require('./side.js');
+const IMMUTABLE = 'Entry instances are immutable';
+
+class Entry {
+  constructor(
+    side,
+    description,
+    accountId,
+    amount
+  ) {
+    this._side = side;
+    this._description = description;
+    this._accountId = accountId;
+    this._amount = amount;
+    return;
+  }
+
+  get side { return this._side }
+  set side { throw Error(IMMUTABLE) }
+  get description { return this._description }
+  set description { throw Error(IMMUTABLE) }
+  get accountId { return this._accountId }
+  set accountId { throw Error(IMMUTABLE) }
+  get amount { return this._amount }
+  set amount { throw Error(IMMUTABLE }
+
+  static decode(json) {
+    let side = null;
+    if (json['side'] == Side.debit.value) {
+      side = Side.debit;
+    } else {
+      side = Side.credit;
+    }
+    const entry = new Entry(
+      side,
+      json['description'],
+      json['account_id'],
+      json['amount']
+    );
+    return entry;
+  }
+
+}
+
+module.exports = Entry;
---
+++ source/side.js
@@ -0,0 +1,13 @@
+/*
+ * Amatino JS
+ * side.js
+ * 
+ * author: hugh@blinkybeach.com
+ */
+
+const Side = {
+  debit: { value: 0, name: 'debit' },
+  credit: { value: 1, name: 'credit' }
+}
+
+module.exports = Side;
