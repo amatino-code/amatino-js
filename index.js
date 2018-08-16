1421fb162867e0d385af19823302845901e244db
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-08-16 18:56:12 +1000

    index new classes

--- index.js
+++ index.js
@@ -7,5 +7,11 @@
  
 module.exports = {
   AmatinoAlpha: require('./source/amatino_alpha.js'),
-  Session: require('./source/session.js')
+  Session: require('./source/session.js'),
+  Account: require('./source/account.js'),
+  Entity: require('./source/entity.js'),
+  Entry: require('./source/entry.js'),
+  Side: require('./source/side.js'),
+  Transaction: require('./source/transaction.js'),
+  Type: require('./source/type.js')
 }
