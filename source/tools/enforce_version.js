09587f5b0d4ddc7550ca8cb17f3d24554e7a316c
Hugh Jeremy <hugh.jeremy@gmail.com>
2019-02-22 15:20:06 +1100

    fix bug causing >10 node to throw version error

--- source/tools/enforce_version.js
+++ source/tools/enforce_version.js
@@ -8,15 +8,18 @@ const package = JSON.parse(filesystem.readFileSync(
 const version = package['engines']['node'];
 const requiredVersion = version.split('=')[1]
 
-const requiredMajor = requiredVersion.split('.')[0];
-const requiredMinor = requiredVersion.split('.')[1];
+const requiredMajor = Number(requiredVersion.split('.')[0]);
+const requiredMinor = Number(requiredVersion.split('.')[1]);
 
 const runningVersion = process.version.split('v')[1];
 
-const runningMajor = runningVersion.split('.')[0];
-const runningMinor = runningVersion.split('.')[1];
+const runningMajor = Number(runningVersion.split('.')[0]);
+const runningMinor = Number(runningVersion.split('.')[1]);
 
-if (runningMajor >= requiredMajor) {
+if (runningMajor > requiredMajor) {
+  process.exit(0);
+}
+if (runningMajor == requiredMajor) {
   if (runningMinor >= requiredMinor) {
     process.exit(0);
   }
